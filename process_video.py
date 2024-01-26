f481f3da644c15013c06ca16fadd7ca7742f82ba
KristinaPopovSV5-2020 <popov.sv5.2020@uns.ac.rs>
2024-01-26 18:57:16 +0100

    added squat dataset

--- process_video.py
+++ process_video.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-number_of_picture = 1
+number_of_picture = 357
 def process_video(video_path, output_folder):
     global number_of_picture
     frame_num = 0
@@ -24,7 +24,7 @@ def process_video(video_path, output_folder):
         # Ako je trenutni frejm svaki deseti frejm
         if frame_num % 10 == 0:
             # Generisanje naziva datoteke za čuvanje skrinšota
-            filename = f"{output_folder}/pushup_{str(number_of_picture)}.png"
+            filename = f"{output_folder}/squat_{str(number_of_picture)}.png"
 
             # Čuvanje skrinšota
             cv2.imwrite(filename, frame)
@@ -33,8 +33,9 @@ def process_video(video_path, output_folder):
     cap.release()
 
 if __name__ == '__main__':
-    path = "data/Correct sequence/"
-    for i in range(1, 51):
-        video_path = path + "Copy of push up " + str(i) + ".mp4"
-        output_path = "yolodemo/Images/PushUp"
+    path = "data/squat/"
+    for i in range(7, 30):
+        video_path = path + "squat_" + str(i) + ".mp4"
+        output_path = "yolodemo/Images/Squat"
         process_video(video_path, output_path)
+    print(number_of_picture)
