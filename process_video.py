fd7916c920afc385dac321cab347470b85277352
KristinaPopovSV5-2020 <popov.sv5.2020@uns.ac.rs>
2024-01-26 20:34:51 +0100

    added crunch dataset

--- process_video.py
+++ process_video.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-number_of_picture = 357
+number_of_picture = 142
 def process_video(video_path, output_folder):
     global number_of_picture
     frame_num = 0
@@ -24,7 +24,7 @@ def process_video(video_path, output_folder):
         # Ako je trenutni frejm svaki deseti frejm
         if frame_num % 10 == 0:
             # Generisanje naziva datoteke za čuvanje skrinšota
-            filename = f"{output_folder}/squat_{str(number_of_picture)}.png"
+            filename = f"{output_folder}/crunch_{str(number_of_picture)}.png"
 
             # Čuvanje skrinšota
             cv2.imwrite(filename, frame)
@@ -33,9 +33,9 @@ def process_video(video_path, output_folder):
     cap.release()
 
 if __name__ == '__main__':
-    path = "data/squat/"
-    for i in range(7, 30):
-        video_path = path + "squat_" + str(i) + ".mp4"
-        output_path = "yolodemo/Images/Squat"
+    path = "data/"
+    for i in range(14, 16):
+        video_path = path + "crunch_" + str(i) + ".mp4"
+        output_path = "yolodemo/Images/Crunch"
         process_video(video_path, output_path)
     print(number_of_picture)
